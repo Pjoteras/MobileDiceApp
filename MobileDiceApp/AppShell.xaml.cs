f8dee09ee9d732b850abc5f5c7abd6743318f11d
Pjoteras <voxli682@gmail.com>
2024-09-23 12:33:43 +0200

    Dodaj pliki projektów.

---
+++ MobileDiceApp/AppShell.xaml.cs
@@ -0,0 +1,7 @@
+﻿namespace MobileDiceApp {
+    public partial class AppShell : Shell {
+        public AppShell() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MobileDiceApp/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿namespace MobileDiceApp {
+    public partial class MainPage : ContentPage {
+        int gameResult = 0;
+
+        public MainPage() {
+            InitializeComponent();
+        }
+
+        private void DiceRoll(object sender, EventArgs e) {
+            //tablica z 5oma liczbami
+            int[] dice = new int[5];
+            //generator liczb
+            Random r = new Random();
+            for (int i = 0; i < 5; i++) {
+                dice[i] = r.Next(1, 7);
+            }
+            Dice0.Source = "k6_" + dice[0] + ".png";
+            Dice1.Source = "k6_" + dice[1] + ".png";
+            Dice2.Source = "k6_" + dice[2] + ".png";
+            Dice3.Source = "k6_" + dice[3] + ".png";
+            Dice4.Source = "k6_" + dice[4] + ".png";
+
+            //wynik rzutu
+            //sprawdzenie powtarzająych się kości
+            int rollResult = 0;
+
+            for (int dots = 1; dots <=6; dots++) {
+                //sprawdza ile jest jakich liczb
+
+                int count = 0;
+
+                for(int diceIndex = 0; diceIndex < 5; diceIndex++) {
+                    if (dice[diceIndex] == dots) {
+                        count++;
+                    }
+                }
+                if(count > 1) {
+                    rollResult += dots * count;
+                }
+                
+            }
+            Label1.Text = "Wynik tego Losowania: " + rollResult;
+            gameResult += rollResult;
+            Label2.Text = "Wynik gry: " + gameResult;
+
+
+
+        }
+
+        private void ResetGame(object sender, EventArgs e) {
+            Dice0.Source = "k6_0.png";
+            Dice1.Source = "k6_0.png";
+            Dice2.Source = "k6_0.png";
+            Dice3.Source = "k6_0.png";
+            Dice4.Source = "k6_0.png";
+        }
+    }
+
+}
